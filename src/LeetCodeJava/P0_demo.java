e0f21a1ac85fb869228c5c5ea70a10f66bc1ffa6
Weisun <1210029395@qq.com>
2020-06-18 21:07:28 +0800

    nothing

--- src/LeetCodeJava/P0_demo.java
+++ src/LeetCodeJava/P0_demo.java
@@ -1,5 +1,14 @@
 package LeetCodeJava;
 
 public class P0_demo {
+    public static void main(String[] args) {
+        int []a ={1,2,3,45};
+        int []b=a.clone();
+        b[2]=4342;
+        for (int i = 0; i <b.length-1; i++) {
+            System.out.println(b[i]);
 
+            System.out.println(a[i]);
+        }
+    }
 }
---
+++ src/LeetCodeJava/P1028_recoverFromPreorder.java
@@ -0,0 +1,32 @@
+package LeetCodeJava;
+
+/**
+ * 输入："1-2--3--4-5--6--7"
+ * 输出：[1,2,5,3,4,6,7]
+ */
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+}
+
+public class P1028_recoverFromPreorder {
+    //从先序遍历还原二叉树
+    public TreeNode recoverFromPreorder(String S) {
+
+        TreeNode ans = null;
+
+
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        P1028_recoverFromPreorder p1028_recoverFromPreorder = new P1028_recoverFromPreorder();
+
+    }
+
+}
