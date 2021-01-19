ff6980ca7f646341107c19c12478b5917e17eed5
Weisun <1210029395@qq.com>
2021-01-19 11:19:49 +0800

    p234会问字题目

---
+++ src/ByteDanceBackend/P0119_234isPalindrome.java
@@ -0,0 +1,39 @@
+package ByteDanceBackend;
+
+
+import java.util.ArrayList;
+import java.util.Stack;
+
+public class P0119_234isPalindrome {
+    class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+
+    // 原本想用栈的，结果发现考虑欠佳，采用复制到数组中，并采用双指针进行判断
+    public boolean isPalindrome(ListNode head) {
+        ArrayList<Integer> list = new ArrayList<>();
+
+        ListNode p=head;
+        while (p!=null){
+            list.add(p.val);
+            p=p.next;
+        }
+
+        //设置双指针
+        int left=0;
+        int right=list.size()-1;
+        while (left<right){
+            if(!list.get(left).equals(list.get(right))){
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+}
