e794e6bbc64eccf9a5fc75b014c5cc279809dd78
Weisun <1210029395@qq.com>
2021-01-31 16:53:22 +0800

    103锯齿链表遍历

--- src/ByteDanceBackend/P02_TreeNode/P0131_103zigzagLevelOrder.java
+++ src/ByteDanceBackend/P02_TreeNode/P0131_103zigzagLevelOrder.java
@@ -1,4 +1,50 @@
 package ByteDanceBackend.P02_TreeNode;
 
+import java.util.*;
+
 public class P0131_103zigzagLevelOrder {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        if (root == null) return new ArrayList<List<Integer>>();
+        //设置一个容器，设置一个flag用来判断左右顺序
+        List<List<Integer>> levelList = new ArrayList<>();
+        int flag = 0;
+        //采用层次遍历法，设置一个栈结构
+        Deque<TreeNode> stack = new LinkedList<TreeNode>();
+        stack.add(root);//头节点放入栈中
+
+        while (!stack.isEmpty()) {
+            int size = stack.size();//当前元素的个数
+            List<Integer> temp = new ArrayList<>();
+            for (int i = 0; i < size; i++) {
+                TreeNode node = stack.pop();
+                if (node.left != null) {
+                    stack.add(node.left);
+                }
+                if (node.right != null) {
+                    stack.add(node.right);//注意左右顺序
+                }
+                if(flag%2==0){
+                    temp.add(node.val);//吐出的元素加入到list中
+                }else {
+                    temp.add(0,node.val);//头插法
+                }
+
+
+            }
+            levelList.add(temp);
+            flag++;
+        }
+
+        return levelList;
+    }
 }
--- src/JianZhi_Offer/P09_CQueue.java
+++ src/JianZhi_Offer/P09_CQueue.java
@@ -8,13 +8,13 @@ public class P09_CQueue {//用两个栈实现队列
 
 class CQueue {
 
-    Deque<Integer>stack1;
-    Deque<Integer>stack2;
+    Deque<Integer> stack1;
+    Deque<Integer> stack2;
 
     public CQueue() {
         //初始化一个队列
-        stack1=new LinkedList<Integer>(); //用来存
-        stack2=new LinkedList<Integer>();//用来吐
+        stack1 = new LinkedList<Integer>(); //用来存
+        stack2 = new LinkedList<Integer>();//用来吐
     }
 
     public void appendTail(int value) {
@@ -22,15 +22,15 @@ class CQueue {
     }
 
     public int deleteHead() {
-        if(!stack2.isEmpty()){
+        if (!stack2.isEmpty()) {
             return stack2.pop();
-        }else {//stack2为空
-            while (!stack1.isEmpty()){
+        } else {//stack2为空
+            while (!stack1.isEmpty()) {
                 stack2.push(stack1.pop());
             }
         }
 
 
-        return stack2.isEmpty()?  -1:stack2.pop();
+        return stack2.isEmpty() ? -1 : stack2.pop();
     }
 }
