417ca6ad74aab30cf60d9710c61120d0a49c88e8
Weisun <1210029395@qq.com>
2021-02-02 12:09:07 +0800

    105二叉树构造

--- src/ByteDanceBackend/P02_TreeNode/P0202_105buildTree.java
+++ src/ByteDanceBackend/P02_TreeNode/P0202_105buildTree.java
@@ -31,19 +31,20 @@ public class P0202_105buildTree {
             inorderList.add(inorder[i]);
         }
 
-        return helper(preorderList,inorderList);
+        return helper(preorderList, inorderList);
     }
-    TreeNode helper(List<Integer> preorder, List<Integer> inorder){
-        if(inorder.size()==0)return null;
+
+    TreeNode helper(List<Integer> preorder, List<Integer> inorder) {
+        if (inorder.size() == 0) return null;
 
         //preoder的第一个就是根节点
-        int rootVal=preorder.remove(0);
+        int rootVal = preorder.remove(0);
         TreeNode root = new TreeNode(rootVal);
 
         //得到根节点，就可以划分左右顺序
         int valIndex = inorder.indexOf(rootVal);
-        root.left=helper(preorder,inorder.subList(0, valIndex));
-        root.right=helper(preorder,inorder.subList(valIndex+1,inorder.size()));
+        root.left = helper(preorder, inorder.subList(0, valIndex));
+        root.right = helper(preorder, inorder.subList(valIndex + 1, inorder.size()));
         return root;
     }
 }
